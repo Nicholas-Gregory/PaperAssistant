93b358d176a75748645d17d5eeb7057adab0c285
Nicholas Gregory <nicholas.babcock092@gmail.com>
2024-09-14 16:52:17 -0700

    Improvement: Page: Auth

--- client/src/layouts/Main.jsx
+++ client/src/layouts/Main.jsx
@@ -1,8 +1,11 @@
 import { Link, Outlet } from 'react-router-dom'
 import TopNav from '../components/TopNav'
 import TopNavLink from '../components/TopNavLink'
+import { useAuth } from '../contexts/UserContext'
 
 export default function Main() {
+    const { user, logout } = useAuth();
+    
     return (
         <>
             <TopNav>
@@ -11,6 +14,14 @@ export default function Main() {
                 <TopNavLink to={'/home'}>Home</TopNavLink>&nbsp; | &nbsp;
                 <TopNavLink to={'/app'}>App</TopNavLink>&nbsp; | &nbsp;
                 <TopNavLink to={'/user'}>User</TopNavLink>&nbsp; | &nbsp;
+                {user._id && (
+                    <span style={{ float: 'right' }}>
+                        Logged in as {user.username}&nbsp;| &nbsp;
+                        <button onClick={logout}>
+                            Logout
+                        </button>
+                    </span>
+                )}
             </TopNav>
             <Outlet />
         </>
--- client/src/layouts/User.jsx
+++ client/src/layouts/User.jsx
@@ -1,3 +1,20 @@
+import { useEffect } from "react";
+import { useNavigate, Outlet } from 'react-router-dom'
+import { useAuth } from "../contexts/UserContext"
+
 export default function User() {
-    return <p>User Layout</p>
+    const { user } = useAuth();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (user._id) {
+            navigate(`/user/${user._id}`, { replace: true });
+        } else {
+            navigate('/user/auth', { replace: true });
+        }
+    }, [user]);
+
+    return (
+        <Outlet />
+    )
 }
